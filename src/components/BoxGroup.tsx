0cdfeee199584b566e35912acdc6279f08d8d947
Jason Gedge <jason@gedge.ca>
2021-03-20 14:17:06 -0400

    Support showing debug boxes on BoxGroup if color in map.
    Specfically, no color has to be set, just the entry key has to exist. This gives
    me a quick and easy way to visualize the box in development.

--- src/components/BoxGroup.tsx
+++ src/components/BoxGroup.tsx
@@ -1,3 +1,4 @@
+import { isNil } from "lodash";
 import React, { useContext } from "react";
 import { HasBox } from "../layout";
 import { svgPositionTransform, svgSizeProps } from "./utils";
@@ -13,19 +14,12 @@ const BUCKETS = [0, 225, 90, 180, 315, 135, 45, 270];
 export function BoxGroup(props: { node: HasBox & { type?: string }; debug?: boolean; children: React.ReactNode }) {
   const debug = useContext(DebugContext);
   let debugColor = null;
-  if (props.debug || debug.enabled) {
-    const newColor = () => {
-      const bucket = BUCKETS[debug.index % BUCKETS.length];
-      const hue = bucket + Math.floor(20 * (Math.random() - 0.5));
-      const saturation = 50 + Math.floor(20 * Math.random());
-      const lightness = 50 + Math.floor(10 * Math.random());
+  const debugType = props.node.type || "undefined";
+  if (props.debug || debug.enabled || debugType in debug.colorMap) {
+    debugColor = debug.colorMap[debugType];
+    if (isNil(debugColor)) {
+      debugColor = debug.colorMap[debugType] = newColor(debug.index);
       debug.index += 1;
-      return `hsl(${hue}deg, ${saturation}%, ${lightness}%)`;
-    };
-
-    debugColor = debug.colorMap[props.node.type || "undefined"];
-    if (debugColor === undefined) {
-      debugColor = debug.colorMap[props.node.type || "undefined"] = newColor();
     }
   }
 
@@ -35,4 +29,12 @@ export function BoxGroup(props: { node: HasBox & { type?: string }; debug?: bool
       {debugColor && <rect {...svgSizeProps(props.node)} fill="none" stroke={debugColor} strokeWidth={0.01} />}
     </g>
   );
+}
+
+function newColor(index: number) {
+  const bucket = BUCKETS[index % BUCKETS.length];
+  const hue = bucket + Math.floor(20 * (Math.random() - 0.5));
+  const saturation = 50 + Math.floor(20 * Math.random());
+  const lightness = 50 + Math.floor(10 * Math.random());
+  return `hsl(${hue}deg, ${saturation}%, ${lightness}%)`;
 }
