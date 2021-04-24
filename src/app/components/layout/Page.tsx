6294810cd32eba64eeb51b009c091c0fa2076711
Jason Gedge <jason@gedge.ca>
2021-04-24 11:10:25 -0400

    Remove hardcoded 8.5' x 11' sizing of pages

--- src/app/components/layout/Page.tsx
+++ src/app/components/layout/Page.tsx
@@ -4,9 +4,14 @@ import LineElementComponent from "../notation/LineElement";
 
 export default function Page(props: { page: Page }) {
   const viewBox = `0 0 ${props.page.width} ${props.page.height}`;
+  const style = {
+    width: `${Math.floor(props.page.width * 10)}rem`,
+    height: `${Math.floor(props.page.height * 10)}rem`,
+  };
+
   return (
     <div className="bg-white border-black border m-4">
-      <svg viewBox={viewBox} style={{ width: "85rem", height: "110rem" }}>
+      <svg viewBox={viewBox} style={style}>
         {/* TODO clip path for the bottom/right margins */}
         <g transform={`translate(${props.page.margins.left} ${props.page.margins.top})`}>
           {props.page.elements.map((e, index) => (
