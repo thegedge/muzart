882bad24e7d6d14739510c7341a92606d712c7c6
Jason Gedge <jason@gedge.ca>
2021-05-10 23:23:29 -0400

    Show an arc for legato slides

--- src/app/components/notation/Slide.tsx
+++ src/app/components/notation/Slide.tsx
@@ -1,8 +1,24 @@
-import React from "react";
-import { LINE_STROKE_WIDTH, Slide } from "../../layout";
+import React, { useMemo } from "react";
+import { Arc as ArcElement, LINE_STROKE_WIDTH, Slide, STAFF_LINE_HEIGHT } from "../../layout";
+import Box from "../../layout/utils/Box";
 import { BoxGroup } from "../layout/BoxGroup";
+import { Arc } from "./Arc";
 
 export function Slide(props: { node: Slide }) {
+  const arc = useMemo<ArcElement>(
+    () => ({
+      type: "Arc",
+      box: new Box(
+        -0.5 * STAFF_LINE_HEIGHT,
+        props.node.box.height + 0.1 * STAFF_LINE_HEIGHT,
+        props.node.box.width + STAFF_LINE_HEIGHT,
+        0.8 * STAFF_LINE_HEIGHT
+      ),
+      orientation: props.node.upwards ? "below" : "above",
+    }),
+    [props]
+  );
+
   return (
     <BoxGroup node={props.node}>
       <line
@@ -10,9 +26,10 @@ export function Slide(props: { node: Slide }) {
         y1={props.node.upwards ? props.node.box.height : 0}
         x2={props.node.box.width}
         y2={props.node.upwards ? 0 : props.node.box.height}
-        stroke="#000000"
+        stroke="#555555"
         strokeWidth={2 * LINE_STROKE_WIDTH}
       />
+      <Arc element={arc} />
     </BoxGroup>
   );
 }
