4e67714950c03d50a4966d4f60f619476c7f9e78
Jason Gedge <jason@gedge.ca>
2022-06-18 17:53:13 -0400

    Update selection when playing to show playback progress

--- src/app/components/ui/SelectionBox.tsx
+++ src/app/components/ui/SelectionBox.tsx
@@ -1,21 +1,46 @@
 import { observer } from "mobx-react-lite";
-import React, { createRef, useEffect, useMemo } from "react";
-import { DEFAULT_MARGIN, LINE_STROKE_WIDTH } from "../../layout";
+import React, { useEffect, useMemo, useRef } from "react";
+import { DEFAULT_MARGIN, LayoutElement, LINE_STROKE_WIDTH } from "../../layout";
 import { Box } from "../../layout/utils/Box";
 import { useApplicationState } from "../utils/ApplicationStateContext";
 import { svgBoxProps } from "../utils/svg";
+
+// TODO factor out these "relative to absolute" calculations in the useMemo calls below
 
 export const SelectionBox = observer(() => {
   const application = useApplicationState();
   const element = application.selection.element;
 
-  const box = useMemo(() => {
+  const measureOffsetBox = useMemo(() => {
+    if (!element) {
+      return;
+    }
+
+    const box = new Box(0, 0, 0, 0); // lazy person's clone
+    let e: LayoutElement<unknown> | undefined = element;
+    while (e && e.box) {
+      box.x += e.box.x;
+      box.y += e.box.y;
+
+      if (e.type == "Measure") {
+        box.width = e.box.width;
+        box.height = e.box.height;
+        break;
+      }
+
+      e = e.parent;
+    }
+
+    return box;
+  }, [element]);
+
+  const elementBox = useMemo(() => {
     if (!element) {
       return;
     }
 
     const box = new Box(0, 0, element.box.width, element.box.height); // lazy person's clone
-    let e = element;
+    let e: LayoutElement<unknown> | undefined = element;
     while (e && e.box) {
       // TODO eliminate need for this by making Page have a single group child properly offset
       if (e.type == "Page") {
@@ -31,7 +56,7 @@ export const SelectionBox = observer(() => {
     return box;
   }, [element]);
 
-  const ref = createRef<SVGRectElement>();
+  const ref = useRef<SVGRectElement | null>();
 
   useEffect(() => {
     if (ref.current) {
@@ -39,15 +64,39 @@ export const SelectionBox = observer(() => {
     }
   }, [element]);
 
-  if (!box) {
+  if (!elementBox || !measureOffsetBox) {
     return null;
   }
 
-  const padding = 3 * LINE_STROKE_WIDTH;
-  box.x -= padding;
-  box.y -= padding;
-  box.width += 2 * padding;
-  box.height += 2 * padding;
+  if (application.playback.playing) {
+    const playbackBox = new Box(
+      elementBox.x,
+      elementBox.y - measureOffsetBox.y,
+      elementBox.width,
+      measureOffsetBox.height
+    );
 
-  return <rect {...svgBoxProps(box)} ref={ref} fill="#f0f0a055" strokeWidth={LINE_STROKE_WIDTH} stroke="#c0c080" />;
+    return (
+      <rect
+        ref={(instance) => {
+          ref.current = instance;
+        }}
+        fill="#ff000033"
+        {...svgBoxProps(playbackBox)}
+      />
+    );
+  } else {
+    const padding = 3 * LINE_STROKE_WIDTH;
+    return (
+      <rect
+        ref={(instance) => {
+          ref.current = instance;
+        }}
+        fill="#f0f0a055"
+        strokeWidth={LINE_STROKE_WIDTH}
+        stroke="#c0c080"
+        {...svgBoxProps(elementBox.expand(padding))}
+      />
+    );
+  }
 });
--- src/playback/PlaybackController.ts
+++ src/playback/PlaybackController.ts
@@ -1,72 +1,20 @@
+import { makeAutoObservable } from "mobx";
+import { Selection } from "../app/components/state/Selection";
 import { Chord, Note, Part } from "../notation";
 import { Sampler } from "./Sampler";
 import { noteValueToSeconds } from "./util/durations";
 
 export class PlaybackController {
+  public playing = false;
   private instrument: Sampler;
   private playbackHandle?: NodeJS.Timeout;
 
   constructor() {
-    // TODO factor out instrument
-    const NOTES = [
-      "A2",
-      "A3",
-      "A4",
-      "A5",
-      "Ab2",
-      "Ab3",
-      "Ab4",
-      "Ab5",
-      "B2",
-      "B3",
-      "B4",
-      "B5",
-      "Bb2",
-      "Bb3",
-      "Bb4",
-      "Bb5",
-      "C#3",
-      "C#4",
-      "C#5",
-      "C#6",
-      "C3",
-      "C4",
-      "C5",
-      "C6",
-      "D3",
-      "D4",
-      "D5",
-      "D6",
-      "E2",
-      "E3",
-      "E4",
-      "E5",
-      "Eb3",
-      "Eb4",
-      "Eb5",
-      "F#2",
-      "F#3",
-      "F#4",
-      "F#5",
-      "F2",
-      "F3",
-      "F4",
-      "F5",
-      "G2",
-      "G3",
-      "G4",
-      "G5",
-    ];
-
-    const noteMap: Record<string, string> = {};
-    for (const note of NOTES) {
-      noteMap[note] = encodeURIComponent(`notes/${note}.mp3`);
-    }
-
-    this.instrument = new Sampler({ urls: noteMap }).toDestination();
+    makeAutoObservable(this, undefined, { deep: false });
+    this.instrument = new Sampler({ urls: NOTES }).toDestination();
   }
 
-  togglePlay(part: Part, startFrom?: { measure?: number; chord?: number }) {
+  togglePlay(part: Part, startFrom?: Selection) {
     if (this.playbackHandle) {
       this.stop();
     } else {
@@ -77,6 +25,11 @@ export class PlaybackController {
         const measure = part.measures[currentMeasure];
         const chord = measure.chords[currentChord];
         const seconds = this.playChord(chord);
+
+        startFrom?.update({
+          measure: currentMeasure,
+          chord: currentChord,
+        });
 
         currentChord += 1;
         if (currentChord >= measure.chords.length) {
@@ -90,6 +43,7 @@ export class PlaybackController {
         this.playbackHandle = setTimeout(playNext, 1000 * seconds);
       };
 
+      this.playing = true;
       playNext();
     }
   }
@@ -98,6 +52,7 @@ export class PlaybackController {
     if (this.playbackHandle) {
       clearTimeout(this.playbackHandle);
       // TODO somehow disconnect the instrument or whatever needs to be done to stop it from playing
+      this.playing = false;
       this.playbackHandle = undefined;
     }
   }
@@ -116,3 +71,56 @@ export class PlaybackController {
     return seconds;
   }
 }
+
+// TODO factor out instrument
+const NOTES = Object.fromEntries(
+  [
+    "A2",
+    "A3",
+    "A4",
+    "A5",
+    "Ab2",
+    "Ab3",
+    "Ab4",
+    "Ab5",
+    "B2",
+    "B3",
+    "B4",
+    "B5",
+    "Bb2",
+    "Bb3",
+    "Bb4",
+    "Bb5",
+    "C#3",
+    "C#4",
+    "C#5",
+    "C#6",
+    "C3",
+    "C4",
+    "C5",
+    "C6",
+    "D3",
+    "D4",
+    "D5",
+    "D6",
+    "E2",
+    "E3",
+    "E4",
+    "E5",
+    "Eb3",
+    "Eb4",
+    "Eb5",
+    "F#2",
+    "F#3",
+    "F#4",
+    "F#5",
+    "F2",
+    "F3",
+    "F4",
+    "F5",
+    "G2",
+    "G3",
+    "G4",
+    "G5",
+  ].map((note) => [note, encodeURIComponent(`notes/${note}.mp3`)])
+);
