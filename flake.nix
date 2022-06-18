8ce87dac8ac2d2766d40d7c5ef0bd807cf6900c9
Jason Gedge <jason@gedge.ca>
2022-06-18 14:44:18 -0400

    Add nix flake

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Muzart development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    (flake-utils.lib.eachDefaultSystem
+      (system: nixpkgs.lib.fix (flake:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          packages = {
+            direnv = pkgs.nix-direnv;
+            nixpkgs-fmt = pkgs.nixpkgs-fmt;
+            nodejs = pkgs.nodejs-18_x;
+            yarn = pkgs.yarn.override {
+              nodejs = flake.packages.nodejs;
+            };
+          };
+
+          devShell = pkgs.mkShell {
+            packages = builtins.attrValues flake.packages;
+          };
+        }
+      )));
+}
