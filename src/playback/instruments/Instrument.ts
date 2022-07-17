39c27bff8b30cf449e188865df298da42b7c6ee6
Jason Gedge <jason@gedge.ca>
2022-07-17 13:58:05 -0400

    Play the entire duration of a tie

--- src/playback/instruments/Instrument.ts
+++ src/playback/instruments/Instrument.ts
@@ -1,7 +1,12 @@
 import { Note } from "../../notation";
 
 export interface Instrument {
+  /** Stop all actively playing notes */
   stop(): void;
+
+  /** Play note at the given tempo */
   playNote(note: Note, tempo: number): void;
+
+  /** Dispose all resources associated with this instrument */
   dispose(): void;
 }
--- src/playback/instruments/SamplerInstrument.ts
+++ src/playback/instruments/SamplerInstrument.ts
@@ -70,42 +70,49 @@ export class SamplerInstrument implements Instrument {
   // TODO figure out if I could just use the regular ToneJS sampler hooked into some output nodes
 
   playNote(note: notation.Note, tempo: number, startTime?: number): number | undefined {
-    if (note.tie && note.tie.type != "start") {
-      return;
-    }
+    const duration = note.dead ? 0.05 : noteValueToSeconds(note.value, tempo);
+    const tieType = note.tie ? note.tie.type : "start";
 
-    const duration = note.dead ? 0.05 : this.tiedNoteDurationSeconds(note, tempo);
     try {
-      const midi = note.pitch.toMidi();
-      const [sample, offset] = this.findClosest(midi);
+      if (tieType == "start") {
+        const midi = note.pitch.toMidi();
+        const [sample, offset] = this.findClosest(midi);
+        const source = this.createToneBufferSource(sample, offset);
 
-      const source = this.createToneBufferSource(sample, offset);
+        // TODO Make these work again
+        this.maybeBend(note, source);
+        this.maybeVibrato(note);
 
-      // TODO Make these work again
-      this.maybeBend(note, source);
-      this.maybeVibrato(note);
+        //---------------------------------------------------------------------------------------------
 
-      //---------------------------------------------------------------------------------------------
+        const volume = this.context.createGain();
 
-      const volume = this.context.createGain();
+        const attack = sample.generators[SoundFontGeneratorType.EnvelopeVolumeAttack];
+        const hold = sample.generators[SoundFontGeneratorType.EnvelopeVolumeHold];
+        const decay = sample.generators[SoundFontGeneratorType.EnvelopeVolumeDecay];
+        const release = sample.generators[SoundFontGeneratorType.EnvelopeVolumeRelease];
+        // const sustain = sample.generators[SoundFontGeneratorType.EnvelopeVolumeSustain];
 
-      const attack = sample.generators[SoundFontGeneratorType.EnvelopeVolumeAttack];
-      const hold = sample.generators[SoundFontGeneratorType.EnvelopeVolumeHold];
-      const decay = sample.generators[SoundFontGeneratorType.EnvelopeVolumeDecay];
-      const release = sample.generators[SoundFontGeneratorType.EnvelopeVolumeRelease];
-      // const sustain = sample.generators[SoundFontGeneratorType.EnvelopeVolumeSustain];
+        this.createEnvelope(volume.gain, { attack, hold, decay, release });
 
-      this.createEnvelope(volume.gain, { attack, hold, decay, release });
+        source.connect(volume);
 
-      source.connect(volume);
+        //---------------------------------------------------------------------------------------------
 
-      //---------------------------------------------------------------------------------------------
+        volume.connect(this.context.destination);
 
-      volume.connect(this.context.destination);
-
-      const computedTime = this.currentTime + (startTime ?? 0);
-      source.start(computedTime, 0, duration);
-      this.addActiveSource(source, volume, note.pitch.toMidi());
+        const computedTime = startTime ? startTime : this.currentTime;
+        source.start(computedTime, 0, note.tie ? undefined : duration);
+        this.addActiveSource(source, volume, note.pitch.toMidi());
+      } else if (tieType == "stop") {
+        const midi = note.pitch.toMidi();
+        const sources = this.activeSources.get(midi);
+        if (sources) {
+          for (const source of sources) {
+            source.audio.stop((startTime ?? this.currentTime) + duration);
+          }
+        }
+      }
     } catch (e) {
       console.warn(e);
     }
@@ -220,21 +227,5 @@ export class SamplerInstrument implements Instrument {
     //   source.playbackRate.linearRampTo(value * ratio, bendPointDuration);
     //   previousTime = time;
     // }
-  }
-
-  private tiedNoteDurationSeconds(note: notation.Note | undefined, tempo: number) {
-    let seconds = 0;
-
-    // TODO get tempo from tab and provide to `noteValueToSeconds` below
-
-    // TODO this is wrong, because the tied note could pass through many other notes, so we need to
-    //      also find out all of the durations in between. Perhaps the easiest way to do this would
-    //      be to keep the note on until we hit the last tie and then turn it off! So easy.
-    while (note) {
-      seconds += noteValueToSeconds(note.value, tempo);
-      note = note.tie?.next;
-    }
-
-    return seconds;
   }
 }
