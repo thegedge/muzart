289b51d33b4b22a2423e70f530355d58adce53dc
Jason Gedge <jason@gedge.ca>
2022-08-06 18:27:33 -0400

    Make Note a layout class

--- src/layout/elements/Chord.ts
+++ src/layout/elements/Chord.ts
@@ -2,12 +2,11 @@ import types, { chordWidth, STAFF_LINE_HEIGHT } from "..";
 import * as notation from "../../notation";
 import { Box, maxMap } from "../utils";
 import { LayoutElement } from "./LayoutElement";
+import { Note } from "./Note";
 
 export class Chord extends LayoutElement<"Chord", types.LineElement> implements types.Chord {
   readonly type = "Chord";
   readonly elements: (types.Note | types.Stroke)[] = [];
-
-  private noteWidth: number;
 
   constructor(readonly chord: notation.Chord) {
     // TODO use num staff lines from ancestor
@@ -15,18 +14,11 @@ export class Chord extends LayoutElement<"Chord", types.LineElement> implements 
     const noteWidth = chordWidth(maxNoteChars);
     super(new Box(0, 0, noteWidth, 6 * STAFF_LINE_HEIGHT));
 
-    this.noteWidth = noteWidth;
-  }
-
-  layout() {
-    for (const note of this.chord.notes) {
+    this.elements = this.chord.notes.map((note) => {
       const noteY = note.placement ? (note.placement.string - 1) * STAFF_LINE_HEIGHT : 0;
-      this.elements.push({
-        type: "Note",
-        parent: this,
-        box: new Box(0, noteY, this.noteWidth, STAFF_LINE_HEIGHT),
-        note,
-      });
-    }
+      const noteElement = new Note(new Box(0, noteY, noteWidth, STAFF_LINE_HEIGHT), note);
+      noteElement.parent = this;
+      return noteElement;
+    });
   }
 }
---
+++ src/layout/elements/Note.ts
@@ -0,0 +1,12 @@
+import types from "..";
+import * as notation from "../../notation";
+import { Box } from "../utils";
+import { LayoutElement } from "./LayoutElement";
+
+export class Note extends LayoutElement<"Note", types.Chord> implements types.Note {
+  readonly type = "Note";
+
+  constructor(box: Box, readonly note: notation.Note) {
+    super(box);
+  }
+}
