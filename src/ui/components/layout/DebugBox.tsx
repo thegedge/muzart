43729f018bcc9f67dd0d365d2a05c1a0e99dd945
Jason Gedge <jason@gedge.ca>
2022-08-07 18:56:35 -0400

    Improve performance of debug mode
    Instead of causing a re-render of many components, we instead capture a ref and `setAttribute`

--- src/ui/components/layout/DebugBox.tsx
+++ src/ui/components/layout/DebugBox.tsx
@@ -1,11 +1,22 @@
-import { observer } from "mobx-react-lite";
-import React, { useMemo } from "react";
+import { autorun } from "mobx";
+import React, { useEffect, useMemo, useRef } from "react";
 import { Box } from "../../../layout";
 import { useApplicationState } from "../../utils/ApplicationStateContext";
 import { svgBoxProps } from "../../utils/svg";
 
-export const DebugBox = observer(function DebugBox(props: { box: Box; debugType: string }) {
+export const DebugBox = (props: { box: Box; debugType: string }) => {
   const { debug } = useApplicationState();
   const debugParams = useMemo(() => debug.paramsForType(props.debugType), [props.debugType]);
-  return <rect {...svgBoxProps(props.box)} {...debugParams} visibility={debug.enabled ? undefined : "hidden"} />;
-});
+  const ref = useRef<SVGRectElement>(null);
+
+  useEffect(() => {
+    return autorun(() => {
+      const visibility = debug.enabled ? "visible" : "hidden";
+      ref.current?.setAttribute("visibility", visibility);
+    });
+  }, [ref]);
+
+  return <rect ref={ref} {...svgBoxProps(props.box)} {...debugParams} visibility="hidden" />;
+};
+
+DebugBox.displayName = "DebugBox";
