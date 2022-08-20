dac768bf8fce1de2a7e1a88f62c2014ed4589b92
Jason Gedge <jason@gedge.ca>
2022-08-20 14:08:17 -0400

    Add `preact/debug` in dev.

--- src/ui/index.tsx
+++ src/ui/index.tsx
@@ -2,9 +2,22 @@ import React from "react";
 import { render } from "react-dom";
 import { App } from "./app";
 
-const root = document.getElementById("root");
-if (!root) {
-  throw new Error("couldn't find #root element");
-}
+const renderApp = async () => {
+  if (import.meta.env.DEV) {
+    // Ugh, linters sometimes... Not sure why TS can't find the declaration file for preact/debug as a dynamic import,
+    // but is totally fine when it's a static import.
+    //
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    await import("preact/debug");
+  }
 
-render(<App />, root);
+  const root = document.getElementById("root");
+  if (!root) {
+    throw new Error("couldn't find #root element");
+  }
+
+  render(<App />, root);
+};
+
+void renderApp();
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,6 +1,13 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
+  // From vite
+  readonly MODE: string;
+  readonly BASE_URL: string;
+  readonly PROD: boolean;
+  readonly DEV: boolean;
+  readonly SSR: boolean;
+
   readonly VITE_DEFAULT_FILE: string;
   readonly VITE_DEFAULT_SOUNDFONT: string;
   readonly VITE_DEBUG_APP: string;
