1b77e14d250589930cbc9fec9de31136bf6b0d23
Jason Gedge <jason@gedge.ca>
2022-10-01 23:20:51 -0400

    Remove shadow from page to dramatically improve performance

--- src/ui/components/score/Page.ts
+++ src/ui/components/score/Page.ts
@@ -1,14 +1,13 @@
 import layout from "../../../layout";
-import { PAGE_MARGIN } from "../../../layout/elements/Part";
 import { Application } from "../../state/Application";
 
 export const Page = (_application: Application, context: CanvasRenderingContext2D, element: layout.Page) => {
-  context.shadowBlur = 2 * PAGE_MARGIN;
-  context.shadowColor = "rgb(0, 0, 0, 0.2)";
+  // context.shadowBlur = 2 * PAGE_MARGIN;
+  // context.shadowColor = "rgb(0, 0, 0, 0.2)";
   context.fillStyle = "#ffffff";
   context.fillRect(element.box.x, element.box.y, element.box.width, element.box.height);
-  context.shadowBlur = 0;
-  context.shadowColor = "";
+  // context.shadowBlur = 0;
+  // context.shadowColor = "";
 
   const box = element.content.box.translate(element.box.x, element.box.y);
   context.beginPath();
