ce8ed263c98da2507edf121c12ed8330d25eda97
Jason Gedge <jason@gedge.ca>
2022-11-12 15:18:30 -0500

    Fix layout for double digit time signatures

--- src/render/TimeSignature.ts
+++ src/render/TimeSignature.ts
@@ -11,21 +11,21 @@ export const TimeSignature = (
   const topDigits = digits(element.timeSignature.count);
   const bottomDigits = digits(element.timeSignature.value.toNumber());
 
-  const size = element.box.width / Math.max(topDigits.length, bottomDigits.length);
-  const spacing = element.box.height - 2 * size;
+  const digitSize = element.box.width / Math.max(topDigits.length, bottomDigits.length);
+  const topBottomGap = element.box.height - 2 * digitSize;
 
   // Height must be `size`, but scale the width proportionally
-  const topDigitElements = topDigits.map((digit) => timeSignatureDigit(digit, undefined, size));
-  const bottomDigitElements = bottomDigits.map((digit) => timeSignatureDigit(digit, undefined, size));
+  const topDigitElements = topDigits.map((digit) => timeSignatureDigit(digit, undefined, digitSize));
+  const bottomDigitElements = bottomDigits.map((digit) => timeSignatureDigit(digit, undefined, digitSize));
 
-  const topOffsetX = 0.5 * (element.box.width - topDigits.length * size);
-  const bottomOffsetX = 0.5 * (element.box.width - bottomDigits.length * size);
+  const topOffsetX = 0.5 * (element.box.width - topDigits.length * digitSize);
+  const bottomOffsetX = 0.5 * (element.box.width - bottomDigits.length * digitSize);
 
   context.fillStyle = "#000000";
 
   topDigitElements.map(({ path, scaleX, scaleY }, index) => {
     context.save();
-    context.translate(element.box.x + topOffsetX + scaleX * (index * size), element.box.y);
+    context.translate(element.box.x + topOffsetX + index * digitSize, element.box.y);
     context.scale(scaleX, scaleY);
     context.fill(new Path2D(path));
     context.restore();
@@ -33,7 +33,7 @@ export const TimeSignature = (
 
   bottomDigitElements.map(({ path, scaleX, scaleY }, index) => {
     context.save();
-    context.translate(element.box.x + bottomOffsetX + scaleX * (index * size), element.box.y + spacing + size);
+    context.translate(element.box.x + bottomOffsetX + index * digitSize, element.box.y + topBottomGap + digitSize);
     context.scale(scaleX, scaleY);
     context.fill(new Path2D(path));
     context.restore();
