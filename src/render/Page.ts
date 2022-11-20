b151485b47f0f4325c7b1511998c8b71ce871ccb
Jason Gedge <jason@gedge.ca>
2022-11-20 16:17:03 -0500

    Bring back page shadows since we're fast again

--- src/render/Page.ts
+++ src/render/Page.ts
@@ -1,13 +1,14 @@
 import layout from "../layout";
+import { PAGE_MARGIN } from "../layout/elements/Part";
 import { Application } from "../ui/state/Application";
 
 export const Page = (_application: Application, context: CanvasRenderingContext2D, element: layout.Page) => {
-  // context.shadowBlur = 2 * PAGE_MARGIN;
-  // context.shadowColor = "rgb(0, 0, 0, 0.2)";
+  context.shadowBlur = PAGE_MARGIN;
+  context.shadowColor = "rgb(0, 0, 0, 0.25)";
   context.fillStyle = "#ffffff";
   context.fillRect(element.box.x, element.box.y, element.box.width, element.box.height);
-  // context.shadowBlur = 0;
-  // context.shadowColor = "";
+  context.shadowBlur = 0;
+  context.shadowColor = "";
 
   const box = element.content.box.translate(element.box.x, element.box.y);
   context.beginPath();
