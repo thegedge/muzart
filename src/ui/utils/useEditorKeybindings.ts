5b029c39985ef1c80d6047152ee44fdfbe9f272f
Jason Gedge <jason@gedge.ca>
2023-09-10 13:00:59 -0400

    Group and name keybindings

--- src/ui/utils/useEditorKeybindings.ts
+++ src/ui/utils/useEditorKeybindings.ts
@@ -12,93 +12,154 @@ export const useEditorKeybindings = () => {
     return () => state.dispose();
   }, [state]);
 
-  useEffect(() => {
-    const listener = createKeybindingsHandler(
-      mapValues(
-        {
-          // Playback ----------------------------------------------------------
-
-          "Space": () => {
+  const keybindingGroups = useMemo(
+    () => ({
+      Playback: {
+        Space: {
+          name: "Toggle Playback",
+          action: () => {
             application.playback.togglePlay();
           },
+        },
+      },
 
-          // Zoom --------------------------------------------------------------
-
-          "$mod+Equal": () => {
+      Zoom: {
+        "$mod+Equal": {
+          name: "Zoom In",
+          action: () => {
             state.setZoom(state.zoom * 1.2);
           },
+        },
 
-          "$mod+Minus": () => {
+        "$mod+Minus": {
+          name: "Zoom Out",
+          action: () => {
             state.setZoom(state.zoom / 1.2);
           },
+        },
 
-          "$mod+0": () => {
+        "$mod+0": {
+          name: "Reset Zoom",
+          action: () => {
             state.setZoom(1);
             state.centerViewportOn();
           },
+        },
+      },
 
-          // Navigation --------------------------------------------------------
-
-          // TODO some of these shouldn't work when playing
-
-          "ArrowLeft": () => {
+      Navigation: {
+        "ArrowLeft": {
+          name: "Previous Chord",
+          action: () => {
             application.selection.previousChord();
           },
-          "ArrowRight": () => {
+        },
+
+        "ArrowRight": {
+          name: "Next Chord",
+          action: () => {
             application.selection.nextChord();
           },
-          "ArrowUp": () => {
+        },
+
+        "ArrowUp": {
+          name: "Higher Note",
+          action: () => {
             application.selection.previousNote();
           },
-          "ArrowDown": () => {
+        },
+
+        "ArrowDown": {
+          name: "Lower Note",
+          action: () => {
             application.selection.nextNote();
           },
+        },
 
-          // TODO should these jump a visible page, or an actual page based on zoom level?
-          "PageDown": () => {
+        // TODO should these jump a visible page, or an actual page based on zoom level?
+        "PageDown": {
+          name: "Next Page",
+          action: () => {
             application.selection.nextPage();
           },
-          "PageUp": () => {
+        },
+
+        "PageUp": {
+          name: "Previous Page",
+          action: () => {
             application.selection.previousPage();
           },
+        },
 
-          "Home": () => {
+        "Home": {
+          name: "First Page",
+          action: () => {
             application.selection.update({ measureIndex: 0 });
           },
-          "End": () => {
+        },
+
+        "End": {
+          name: "Last Page",
+          action: () => {
             const part = application.selection.part;
             application.selection.update({ measureIndex: part && part.part.measures.length - 1 });
           },
+        },
 
-          "$mod+Shift+ArrowLeft": () => {
+        "$mod+Shift+ArrowLeft": {
+          name: "Previous Measure",
+          action: () => {
             application.selection.previousMeasure();
           },
-          "$mod+Shift+ArrowRight": () => {
+        },
+
+        "$mod+Shift+ArrowRight": {
+          name: "Next Measure",
+          action: () => {
             application.selection.nextMeasure();
           },
+        },
 
-          "$mod+Alt+ArrowUp": () => {
+        "$mod+Alt+ArrowUp": {
+          name: "Previous Part",
+          action: () => {
             application.selection.previousPart();
           },
-          "$mod+Alt+ArrowDown": () => {
+        },
+
+        "$mod+Alt+ArrowDown": {
+          name: "Next Part",
+          action: () => {
             application.selection.nextPart();
           },
+        },
+      },
 
-          // Debugging ---------------------------------------------------------
-
-          "Shift+D": () => {
+      Debugging: {
+        "Shift+D": {
+          name: "Toggle Debug View",
+          action: () => {
             application.debug.setEnabled(!application.debug.enabled);
           },
         },
-        preventDefault,
-      ),
+      },
+    }),
+    [application, state],
+  );
+
+  useEffect(() => {
+    const actions = Object.fromEntries(
+      Object.values(keybindingGroups)
+        .flatMap((group) => Object.entries(group))
+        .map(([key, { action }]) => [key, action]),
     );
+    const listener = createKeybindingsHandler(mapValues(actions, preventDefault));
 
     document.body.addEventListener("keydown", listener);
     return () => {
       document.body.removeEventListener("keydown", listener);
     };
-  }, [application, state]);
+  }, [keybindingGroups]);
 
   return state;
 };
