3cb7857919fba4a7bdb201e4b75f9b9ef8036171
Jason Gedge <jason@gedge.ca>
2024-09-28 14:43:06 -0400

    Add backdrop blur to part listing header

--- packages/editor/src/ui/editor/part-panel/PartListingArea.tsx
+++ packages/editor/src/ui/editor/part-panel/PartListingArea.tsx
@@ -1,8 +1,10 @@
 import type { Score } from "@muzart/notation";
+import clsx from "clsx";
 import { observer } from "mobx-react-lite";
 import { PartListingRow } from "./PartListingRow";
 
 export const PartListingArea = observer((props: { score: Score }) => {
+  const headerClasses = clsx("sticky top-0 backdrop-blur-2xs items-center flex bg-inherit");
   return (
     <div
       className="sticky left-0 z-50 grid items-stretch gap-y-px bg-gray-950/75 pr-px"
@@ -12,10 +14,10 @@ export const PartListingArea = observer((props: { score: Score }) => {
         gridTemplateRows: "2rem", // h-8
       }}
     >
-      <div className="sticky top-0 flex w-8 items-center justify-center bg-inherit p-0.5 text-gray-500" />
-      <div className="sticky top-0 col-start-2 flex items-center bg-inherit px-2">Track</div>
-      <div className="sticky top-0 flex w-7 items-center justify-center bg-inherit p-0.5 text-gray-500" />
-      <div className="sticky top-0 flex w-7 items-center justify-center rounded-r bg-inherit p-0.5 text-gray-500" />
+      <div className={headerClasses} />
+      <div className={clsx(headerClasses, "px-2")}>Track</div>
+      <div className={headerClasses} />
+      <div className={headerClasses} />
       {props.score.parts.map((part, partIndex) => {
         return <PartListingRow key={partIndex} part={part} />;
       })}
